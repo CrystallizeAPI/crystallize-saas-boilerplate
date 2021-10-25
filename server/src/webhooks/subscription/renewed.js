9667be01d744d8527f0e3ef5a93677467bffd393
Alex Luong <alex@alexluong.com>
2021-10-24 22:22:12 -0700

    feat: logic for renew + change plan + checkout with Tillit & Stripe

--- server/src/webhooks/subscription/renewed.js
+++ server/src/webhooks/subscription/renewed.js
@@ -1,23 +1,38 @@
-const emailService = require("../../services/email-service");
-const { orders, subscriptions } = require("../../services/crystallize");
+const crystallize = require("../../services/crystallize");
 
 module.exports = async function subscriptionRenewed(payload) {
   console.log("Webhook payload: subscriptionRenewed");
   console.log(JSON.stringify(payload, null, 2));
 
-  const subscription = await subscriptions.get(payload.id);
+  const subscription = await crystallize.subscriptions.get(payload.id);
 
-  console.log(JSON.stringify(subscription, null, 2));
+  // console.log(JSON.stringify(subscription, null, 2));
 
-  await emailService.sendOrderConfirmation(order);
-
-  if (!order.meta.find((p) => p.key === "isFirstOrder")?.value === "1") {
-    return;
-  }
-
-  await subscriptions.create(
-    `/pricing-page/${order.cart[0].sku}`,
-    order.customer.addresses[0].email,
-    order.payment[0]
+  const customer = await crystallize.customers.get({
+    identifier: subscription.customerIdentifier,
+  });
+  const product = await crystallize.products.getByPath(
+    `/pricing-page/${subscription.item.sku}`
   );
+  const payment = subscription.payment;
+  const item = subscription.item;
+
+  console.log("Charge");
+  const charge = await crystallize.utils.createCharge({
+    payment,
+    item,
+    customer,
+    totalValue: subscription.status.price,
+    currency: subscription.status.currency,
+  });
+
+  const orderInput = crystallize.utils.createOrderInput({
+    customer,
+    payment,
+    charge,
+    product,
+  });
+
+  console.log("Create Crystallize Order");
+  await crystallize.orders.create(orderInput);
 };
